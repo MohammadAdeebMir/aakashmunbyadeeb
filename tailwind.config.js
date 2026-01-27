099283bb571b2babd7e591770e84a088462f3596
Mohammad Adeeb Mir <80444771+MohammadAdeebMir@users.noreply.github.com>
2026-01-28 00:13:59 +0530

    Update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#87ceeb', // Updated primary color to light blue
+      },
+    },
+  },
+  plugins: [],
+}
